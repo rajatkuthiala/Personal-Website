abd0f8418c9feb55dd716cb2447e08678eae4cd0
Rajat Kuthaiala <rajatkuthiala@gmail.com>
2018-10-10 08:50:37 +0000

    Fixed the old commits

--- includes/constant.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//Define constants Here
-
-define('DB_SERVER', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'Galaxys#4');
-define('DB_NAME', 'sys');
--- layout/footer.php
+++
@@ -1,9 +0,0 @@
-<footer id="signature" class="container-fluid">
-  <div class="row">
-    <div class="col-md-6 col-md-offset-3">
-      <center><h4> Designed by Rajat Kuthiala </h4>
-        <h4>2018</h4>
-    </center>
-    </div>
-  </div>
-</footer>
